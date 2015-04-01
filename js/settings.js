d91d86806b30243db05adef735717832bca1def9
Gavin Delphia <magicgavin2004@gmail.com>
2015-03-31 23:36:37 -0600

    Initial Commit
    Simplifies the process of patching a Citrix XenServer. Currently in a
    testing state for a local setup so there are some hard-coded
    preferences.

---
+++ js/settings.js
@@ -0,0 +1,8 @@
+var settings = {
+	updatefile: "http://70.42.241.149/XenServer/updates.xml",
+	user: "root",
+	password: "xenpass",
+	port: 443,
+	ip: "10.0.0.21",
+	patchsavedir: "\\\\10.0.0.40\\CitrixUpdate\\patches\\"
+};
